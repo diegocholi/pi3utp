331322a57cf43af9cf5d3df3a692999cad67d8a0
Diego <diego.choli@hotmail.com>
2019-05-25 01:04:04 -0300

    Att: Fotos sistema 0.1

---
+++ controller/loginController.js
@@ -0,0 +1,13 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+function shine() {
+    document.getElementById("shine").style.opacity = "1";
+}
+
+function turnOff(){
+    document.getElementById("shine").style.opacity = "0.5";
+}
